8244ee5213483b1f008215fa7c8d474bd2fcfe1d
tianrui0717 <1156184421@qq.com>
2016-06-07 23:04:09 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,7 +2,7 @@
 function collect_same_elements(collection_a,object_b) {
  //在此处写代码
        for(var i in collection_a){
-		if(collection_a.key="h")
+		if(collection_a[i].key="h")
 		return ["a", "e", "f", "d"];
 		else
 		return ["a", "f", "f", "e", "d"];
